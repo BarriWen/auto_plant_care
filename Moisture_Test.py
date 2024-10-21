deb43b9b173a9f649069a2ffc20e0e1f7c2ad22a
BarriWen <wenzelongkyrie@outlook.com>
2024-10-21 18:24:23 -0400

    update humidity calculation

--- Moisture_Test.py
+++ Moisture_Test.py
@@ -7,8 +7,10 @@ moisture_sensor = ADC(Pin(26))
 try:
     while True:
         # Read the analog value from the sensor
-        sensor_value = (moisture_sensor.read_u16() / 65535.0) * 100
-        print(sensor_value)
+        # sensor_data = round((1 - (moisture_sensor.read_u16() / 65535.0)) * 100, 2)
+        sensor_data = sensor_pin.read_u16()
+        humidity_percent = round((1 - (sensor_data / 65535)) * 100, 2)
+        print("Humidity: {}%".format(humidity_percent))
         time.sleep(0.25)
 
 except KeyboardInterrupt:
