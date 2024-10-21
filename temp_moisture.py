da341ac3c21588936f1de647ee8102b836ab6606
Billy-FIN <billyfinnn@gmail.com>
2024-10-21 18:55:06 -0400

    improved the code

--- temp_moisture.py
+++ temp_moisture.py
@@ -1,34 +1,20 @@
 import machine
 import time, utime
 from machine import I2C, Pin
+from dht20 import DHT20
 
-soil_humidity_adc = machine.ADC(26)
-i2c = I2C(0, scl=Pin(5), sda=Pin(4), freq=100000)
-
-devices = i2c.scan()
-
-if devices:
-    print("I2C device(s) found:")
-    for device in devices:
-        print(f"Device at address: {hex(device)}")
-else:
-    print("No I2C devices found.")
-
-def read_temp():
-    i2c.writeto(0x38, b'\xF5')  # Send measurement command
-    time.sleep(0.5)
-    data = i2c.readfrom(0x38, 2)  # Read data
-    temperature = data[0]
-    return temperature
+moisture_sensor = machine.ADC(26)
+i2c0 = I2C(0, scl=Pin(5), sda=Pin(4), freq=100000)
+temp_sensor = DHT20(0x38, i2c0)
 
 try:
     while True:
-        raw_value = soil_humidity_adc.read_u16()
-    
-        humidity_percentage = (raw_value / 65535.0) * 100
-        print(f"Soil Moisture: {humidity_percentage:.2f}%")
-        
-        temp = read_temp()
+        # measure moisture
+        sensor_data = moisture_sensor.read_u16()
+        humidity_percent = round((1 - (sensor_data / 65535)) * 100, 2)
+        print(f"Soil Moisture: {humidity_percent:.2f}%")
+        # measure temp
+        temp = temp_sensor.measurements['t']
         print(f'Temperature: {temp} °C')
         
         utime.sleep(1)
