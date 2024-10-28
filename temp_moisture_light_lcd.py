8df517121d8c1e4c8661f3354d1217fe3e077a85
Ivy Zhang <ivyzhang@ivydeair.dyn.brandeis.edu>
2024-10-28 18:39:14 -0400

    add/combine_light_lcd

--- temp_moisture_light_lcd.py
+++ temp_moisture_light_lcd.py
@@ -1,14 +1,22 @@
 import machine
 import time, utime
 from machine import I2C, Pin
+from lcd1602 import LCD1602
 from dht20 import DHT20
+
+i2c = I2C(1,scl=Pin(7), sda=Pin(6), freq=400000)
+d = LCD1602(i2c, 2, 16)
+d.display()
 
 moisture_sensor = machine.ADC(26)
 i2c0 = I2C(0, scl=Pin(5), sda=Pin(4), freq=100000)
 temp_sensor = DHT20(0x38, i2c0)
+light_sensor = machine.ADC(27)
 
 try:
     while True:
+        d.clear()
+        
         # measure moisture
         sensor_data = moisture_sensor.read_u16()
         humidity_percent = round((1 - (sensor_data / 65535)) * 100, 2)
@@ -16,8 +24,19 @@ try:
         # measure temp
         temp = temp_sensor.measurements['t']
         print(f'Temperature: {temp} °C')
+        # measure light
+        light_value = light_sensor.read_u16()
+        print("Light Sensor Value:", light_value)
         
-        utime.sleep(1)
+        d.print(f'Moisture: {humidity_percent:.2f}%')
+        d.setCursor(0, 1)
+        d.print(f'Temp: {temp:.2f} *C')
+        utime.sleep(3)
+        d.clear()
+        d.setCursor(0, 0)
+        d.print(f'Light:{light_value}')
+        
+        utime.sleep(5)
 
 except KeyboardInterrupt:
     print("End")
